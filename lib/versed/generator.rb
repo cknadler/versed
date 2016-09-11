b122d27581ba00f83808619ea69ae85a08563771
Chris Knadler <kndlrc@amazon.com>
2016-09-10 21:31:54 -0700

    It works, but holy shit I'm bad at this now

--- lib/versed/generator.rb
+++ lib/versed/generator.rb
@@ -1,3 +1,5 @@
+require "date"
+require "mustache"
 require "versed/parser"
 require "versed/reader"
 
@@ -9,13 +11,61 @@ module Versed
     # converts the HTML to a PDF.
     def self.run(schedule_path, log_path)
       schedule = Versed::Reader.read(schedule_path)
-      log = Versed::Reader.read(log_path)
+      raw_log = Versed::Reader.read(log_path)
 
-      puts "Schedule"
-      puts schedule
+      logs = []
+      raw_log.each do |day, tasks|
+        logs[Date.parse(day).wday] = tasks
+      end
 
-      puts "Log"
-      puts log
+      ###
+      # Getting row headers
+      ###
+
+      all_tasks = []
+      schedule.each do |day, tasks|
+        all_tasks += tasks.keys
+      end
+      all_tasks.uniq!
+      all_tasks.sort!
+
+      weekdays = [
+        "sunday",
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday"
+      ]
+
+      ###
+      # constructing table
+      ###
+
+      table = {
+        "headers" => weekdays,
+        "rows" => []
+      }
+
+      all_tasks.each do |task_id|
+        row = {}
+        row["row_head"] = task_id
+        row["values"] = []
+
+        logs.each do |tasks|
+          value = nil
+          if tasks && tasks[task_id]
+            value = tasks[task_id]
+          end
+          row["values"] << value
+        end
+
+        table["rows"] << row
+      end
+
+      path = File.expand_path(File.join(__FILE__, "../../../templates/table.mustache"))
+      puts Mustache.render(IO.read(path), table)
     end
   end
 end
